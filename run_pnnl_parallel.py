0d969cc3445e41f227106bd408bbf0fc9c39a22f
realgyeongminnoh <realgyeongminnoh@gmail.com>
2025-05-29 11:52:47 +0900

    data is complete

--- run_pnnl_parallel.py
+++ run_pnnl_parallel.py
@@ -17,6 +17,8 @@ def parse_args():
     parser.add_argument("--time_horizon_idx_smp_ref", "--thi_smp_ref", type=int, default=0)
     # solver bool flags
     parser.add_argument("--use_smp_ref", "--usr", action="store_true")
+    # others
+    parser.add_argument("--only_lfp", "--lfp", action="store_true")
 
     return parser.parse_args()
 
@@ -25,9 +27,9 @@ def get_smp_monthly(args):
     timestamp = np.load(Path(__file__).resolve().parents[0] / "data" / "inputs" / f"time_horizon_{args.time_horizon_idx_smp}" / "timestamp.npy")
     timestamp_monthly = np.arange(timestamp[0].astype("datetime64[M]"), (timestamp[-1] + 1).astype("datetime64[M]"))
 
-    global timestamp_monthly_str, num_months, num_years, smp_monthly, smp_ref_monthly
-    timestamp_monthly_str = timestamp_monthly.astype(str).tolist()
-    num_months = len(timestamp_monthly_str)
+    global timestamp_yearly_str, num_months, num_years, smp_monthly, smp_ref_monthly
+    timestamp_yearly_str = np.arange(timestamp[0].astype("datetime64[Y]"), (timestamp[-1] + 1).astype("datetime64[Y]")).astype(str).tolist()
+    num_months = len(timestamp_monthly)
     num_years = num_months // 12
     smp_monthly = [
         get_smp(args.time_horizon_idx_smp, time_month_start, time_month_end)
@@ -56,6 +58,7 @@ def per_csv(path_inputs_csv_file, path_outputs_csv_file_profit, path_outputs_csv
     arbitrage_profit = np.empty((num_configs, num_months))
     discharging_revenue = np.empty((num_configs, num_months))
 
+    # loop by config
     for idx_config, arr_config in enumerate(arr_configs):
         
         global parameter
@@ -73,21 +76,24 @@ def per_csv(path_inputs_csv_file, path_outputs_csv_file_profit, path_outputs_csv
             rad=int(arr_config[5]),
         )
 
+        # parallel by month
         with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
             outputs = list(
                 executor.map(job_per_processor, range(num_months))
             )
  
+        # save on ram by config
         arbitrage_profit[idx_config] = [output.arbitrage_profit for output in outputs]
         discharging_revenue[idx_config] = [output.discharging_revenue for output in outputs]
 
+    # save on disk by config x year
     pd.DataFrame(
-        arbitrage_profit.reshape(num_configs, num_years, 12).sum(axis=2),
-        columns=timestamp_monthly_str,
+        arbitrage_profit.reshape(num_configs, num_years, 12).sum(axis=2).astype(int),
+        columns=timestamp_yearly_str,
     ).to_csv(path_outputs_csv_file_profit, index=False)
     pd.DataFrame(
-        discharging_revenue.reshape(num_configs, num_years, 12).sum(axis=2),
-        columns=timestamp_monthly_str,
+        discharging_revenue.reshape(num_configs, num_years, 12).sum(axis=2).astype(int),
+        columns=timestamp_yearly_str,
     ).to_csv(path_outputs_csv_file_revenue, index=False)
 
 
@@ -100,13 +106,17 @@ def main():
 
     names_csv_file = [
         "Lithium-ion_LFP_2023_Neutral.csv",
-        # "Lithium-ion_LFP_2030_Neutral.csv",
-        # "Lithium-ion_LFP_2030_Optimistic.csv",
-        # "Lithium-ion_LFP_2030_Pessimistic.csv",
-        # "Lithium-ion_NMC_2023_Neutral.csv",
-        # "Lead_Acid_2023_Neutral.csv",
-        # "Vanadium_Redox_Flow_2023_Neutral.csv",
+        "Lithium-ion_LFP_2030_Neutral.csv",
+        "Lithium-ion_LFP_2030_Optimistic.csv",
+        "Lithium-ion_LFP_2030_Pessimistic.csv",
     ]
+
+    if not args.only_lfp:
+        names_csv_file += [
+            "Lithium-ion_NMC_2023_Neutral.csv",
+            "Lead_Acid_2023_Neutral.csv",
+            "Vanadium_Redox_Flow_2023_Neutral.csv",
+        ]
 
     path_inputs_pnnl_folder = Path.cwd() / "data" / "inputs" / "pnnl"
     path_outputs_pnnl_folder = (
@@ -115,6 +125,7 @@ def main():
     )
     path_outputs_pnnl_folder.mkdir(parents=True, exist_ok=True)
 
+    # loop by csv
     for name_csv_file in names_csv_file:
         path_inputs_csv_file = path_inputs_pnnl_folder / name_csv_file
         path_outputs_csv_file_profit = path_outputs_pnnl_folder / (name_csv_file[:-4] + "_profit.csv")
